956e850fdf4be653eebf87f1e17db141237bd03f
ryan4waters <34560283+ryan4waters@users.noreply.github.com>
2021-11-02 16:00:14 +0800

    Create hash_table.cpp

---
+++ LC-Two Sum/hash_table.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashtable;
+        for (int i = 0; i < nums.size(); ++i) {
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end()) {
+                return {it->second, i};
+            }
+            hashtable[nums[i]] = i;
+        }
+        return {};
+    }
+};
