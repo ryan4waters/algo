8d6b75ce2864252e534dc1ead440e93a06985c87
ryan4waters <34560283+ryan4waters@users.noreply.github.com>
2021-11-02 15:36:03 +0800

    Create print_by_row.cpp

---
+++ Two-dimensional grid printing/print_by_row.cpp
@@ -0,0 +1,25 @@
+int main(){
+  int n,m;
+  cin>>n>>m;
+  string s1="+---";
+  string s2="|   ";
+
+  for(int j=0;j<(2*n+1);j++){
+
+  if((j%2)==0){
+  for(int i=0;i<m;i++){
+    cout<<s1;
+  }
+  cout<<"+"<<endl;
+  }
+
+  else{
+    for(int k=0;k<m;k++){
+    cout<<s2;
+    }
+    cout<<"|"<<endl;
+  }
+  }
+
+  system ("pause");
+}
