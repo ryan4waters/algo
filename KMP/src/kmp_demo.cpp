9b11434249b53633e85edd7612d2fe9d11db3132
ryan4waters <ryan4waters@163.com>
2023-01-20 20:44:29 +0800

    update directory and files

---
+++ KMP/src/kmp_demo.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+class KMP
+{
+public:
+    void GetKMPNextArrInit(const string& ss) {
+        next.resize(ss.size(),0);
+        for (int sssr = ss.size() - 1; sssr > 0; --sssr) {
+            for (int sssl = sssr; sssl > 0; --sssl) {
+                int i = 0;
+                for (int pos = sssl; pos <= sssr; ++pos, ++i) {
+                    if (ss[i] != ss[pos]) {
+                        break;
+                    }
+                }
+                if (i == sssr - sssl + 1) {
+                    next[sssr] = max(next[sssr],i);
+                }
+            }
+        }
+    }
+
+    void GetKMPNextArr(const string& ss) {
+        this->next.resize(ss.size(), 0);
+        int left = 0, right = 1;
+        while (right < ss.size()) {
+            if (ss[right] == ss[left]) {
+                ++left;
+                next[right] = left;
+                ++right;
+            } else if (left != 0) {
+                left = next[left -1];
+            } else {
+                next[right] = 0;
+                ++right;
+                
+            }
+        }
+    }
+
+    vector<int> GetMasterStringStartIdx(const string& ms, const string& ss) {
+        GetKMPNextArr(ss);
+        int midx = 0, sidx = 0;
+        vector<int>ret;
+
+        while (midx < ms.size()) {
+            if (ms[midx] == ss[sidx]) {
+                ++midx;
+                ++sidx;
+            } else if (sidx != 0) {
+                sidx = next[sidx - 1];
+            } else {
+                ++midx;
+            }
+
+            if (sidx == ss.size()) {
+                ret.emplace_back(midx - sidx);
+                sidx = next[sidx - 1];
+            }
+        }
+        return ret;
+    }
+private:
+    vector<int>next;
+};
+
+int main()
+{
+    string ms = "abcdabdcdaabcda";
+    string ss = "abc";
+    KMP mykmp;
+    vector<int>v = mykmp.GetMasterStringStartIdx(ms, ss);
+    for_each(v.begin(), v.end(), [](int val) {
+        cout << val << " ";
+    }); cout << endl;
+
+    return 0;
+}
